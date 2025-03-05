cd0d82a83789ca6c646282d692393e35360462d2
aditya raut <Aditya Raut>
2025-03-05 15:08:38 +0530

    SpiralMatrix

---
+++ spiralMatrix.java
@@ -0,0 +1,53 @@
+
+class spiralMatrix {
+
+    public static void spiralMatrix(int mat[][]){
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = mat.length-1;
+        int endCol = mat[0].length-1;
+        
+        while(startRow <= endRow && startCol <= endCol){
+            //top
+            for(int i=startCol ; i<=endCol ; i++){
+                System.out.print(mat[startRow][i]+" ");
+            }
+
+            // right
+            for(int j=startRow+1 ; j<=endRow ; j++){
+                System.out.print(mat[j][endCol]+" ");
+            }
+
+            //bottom
+            for(int i=endCol-1 ; i>=startCol ; i--){
+                if(startRow == endRow){
+                    break;
+                }
+                System.out.print(mat[endRow][i]+" ");
+            }
+
+            //left
+            for(int j=endRow-1 ; j>=startRow+1 ; j--){
+                if(startCol == endCol){
+                    break;
+                }
+                System.out.print(mat[j][startCol]+" ");
+            }
+
+            startRow++;
+            endRow--;
+            startCol++;
+            endCol--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int mat[][] = {
+            {1, 5, 0},
+            {1, 5, 0},
+            {1, 5, 0}
+        };
+
+        spiralMatrix(mat);
+    }
+}
